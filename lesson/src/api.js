0f6aeed51ba83ad921c1e0b465d0b37502eb9bee
Xostron <aat9044107480@gmail.com>
2023-02-04 14:32:54 +0300

    init project

---
+++ lesson/src/api.js
@@ -0,0 +1,75 @@
+const API_KEY=
+'d13ace045ea70c31c4079e05c79a4ef71b3527b1a0f1cd5ea49b0d88d6b6ba4e'
+// подписка на tickets
+const tickersHandlers = new Map(); // {}
+const socket = new WebSocket(
+  `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
+);
+
+const AGGREGATE_INDEX = "5";
+
+socket.addEventListener("message", e => {
+  const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice } = JSON.parse(
+    e.data
+  );
+  if (type !== AGGREGATE_INDEX || newPrice === undefined) {
+    return;
+  }
+
+  const handlers = tickersHandlers.get(currency) ?? [];
+  handlers.forEach(fn => fn(newPrice));
+});
+
+function sendToWebSocket(message) {
+  const stringifiedMessage = JSON.stringify(message);
+
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(stringifiedMessage);
+    return;
+  }
+
+  socket.addEventListener(
+    "open",
+    () => {
+      socket.send(stringifiedMessage);
+    },
+    { once: true }
+  );
+}
+
+function subscribeToTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubAdd",
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+}
+
+function unsubscribeFromTickerOnWs(ticker) {
+  sendToWebSocket({
+    action: "SubRemove",
+    subs: [`5~CCCAGG~${ticker}~USD`]
+  });
+}
+
+export const subscribeToTicker = (ticker, cb) => {
+  const subscribers = tickersHandlers.get(ticker) || [];
+  tickersHandlers.set(ticker, [...subscribers, cb]);
+  subscribeToTickerOnWs(ticker);
+};
+
+export const unsubscribeFromTicker = ticker => {
+  tickersHandlers.delete(ticker);
+  unsubscribeFromTickerOnWs(ticker);
+};
+
+// получить список существующих тикетов
+
+export function getSummaryTickets(){
+  fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true')
+  .then(r=>r.json())
+  .then((rawData)=>{
+const summary = Object.keys(rawData.Data)
+console.log(summary)
+// console.log(Object.values(summary).map(val=>[val.Symbol,val.FullName]))
+  })
+}
